d5495dcc1f099dd75b64352f6d04bcf6305c4649
Ahn <Ahn@MacBookPro.local>
2016-04-22 12:59:31 +0900

    commit

---
+++ src/main/java/com/zuperztar/App.java
@@ -0,0 +1,10 @@
+package com.zuperztar;
+
+/**
+ * Algorithm On Java
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Algorithm On Java");
+    }
+}
